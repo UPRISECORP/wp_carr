78ac43232f4e0e96277c610c6d925ee637e4920e
SJCC25 <kcarrl@yahoo.com.mx>
2025-02-02 22:40:33 -0600

    settings_confif

--- my_portafolio/my_portafolio/settings.py
+++ my_portafolio/my_portafolio/settings.py
@@ -9,24 +9,37 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+# ADD CARR
+import os
+from decouple import config, Csv
+# ADD CARR
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
+# CARR
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: don't run with debug turned on in production!
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = config('SECRET_KEY') # Here
+DEBUG = config('DEBUG', cast = bool) # Here
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+# CARR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i_3h$^&xi6d!ek!qdq_+m&i$fa$$cwl@48m9-=()1+_o3q5adm'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+#CARR
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast = int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast = bool)
+#CARR
 
 # Application definition
 
@@ -37,7 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio',  # Nuestra app
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,12 +88,22 @@ WSGI_APPLICATION = 'my_portafolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+#CARR
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+#CARR
 
 
 # Password validation
--- my_portafolio/portfolio/models.py
+++ my_portafolio/portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    titulo = models.CharField(max_length=250)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='proyectos/')
+    enlace = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
+clear
+
+
